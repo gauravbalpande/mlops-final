3335cf2ef37b8d5ef916c9467fb37a396a94e8ae
Gaurav Balpande <gauravbalpande18@gmail.com>
2025-12-28 17:36:18 +0530

    lets start project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is the project where i am going to create a production based mlops project.',
+    author='Gaurav Balpande',
+    license='MIT',
+)
