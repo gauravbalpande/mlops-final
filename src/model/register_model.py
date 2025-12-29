c9c5ed76672545b6e82cacd14c57d615b1999447
Gaurav Balpande <gauravbalpande18@gmail.com>
2025-12-30 00:02:35 +0530

    some more changers last one hope more almost

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -26,6 +26,8 @@ os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 dagshub_url = "https://dagshub.com"
 repo_owner = "gauravbalpande"
 repo_name = "mlops-final"
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 # -------------------------------------------------------------------------------------
 # Below code is for local use
